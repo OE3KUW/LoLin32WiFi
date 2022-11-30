0bbb2f2fb05217b73429d13d4153cdc243681cc6
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2022-11-30 01:42:56 +0100

    first useful version

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,13 @@
 #define MOTOR_R                          32      // GPIO 32
 #define MOTOR_R_DIR                      33      // GPIO 33
 
-#define REFV                             50.     // factor
+#define REFV                             68.50     // factor
+#define LEN                              200
+#define N                                10
 
 AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
 
 const char* inputText = "inputText";
 const char* speedLeft = "speedLeft";
@@ -60,16 +64,25 @@ const char index_html[] = R"(
         h2 {font-size: 2.0rem; color: #00cc00;}
     </style>
     <script>
-        function message_popup()
+        if (!!window.EventSource) 
         {
-            setTimeout(function() {document.location.reload(false); }, 500);
+            var source = new EventSource('/events');
+            source.addEventListener('open', function(e) { console.log("Events Connected");   }, false);
+            source.addEventListener('error', function(e) {
+                if (e.target.readyState != EventSource.OPEN) { console.log("Events Disconnected"); }
+                                                                                             }, false);
+            source.addEventListener('message', function(e) { console.log("message", e.data); }, false);
+
+            source.addEventListener('adc', function(e) { console.log("adc", e.data);
+                document.getElementById("adc").innerHTML = e.data;                          }, false);
         }
     </script>
     </head>
     <body>
     <h2>HTL St.P&ouml;lten :: EL</h2>
     <br>
-    battery-levl: %input_adc% Volt
+    battery-levl: <span id="adc">%adc%</span> Volt
+    <br>
     <br>
     <form action="/get" target="hidden-form">
         Enter string: <input type="text" name="inputText">
@@ -131,7 +144,6 @@ String processor(const String& var)
 {
     if     (var == "inputText")    { return read_file(SPIFFS, "/input_Text.txt");    }  
     else if(var == "input_speedL") { return read_file(SPIFFS, "/input_speedL.txt");  }  
-    else if(var == "input_adc")    { return read_file(SPIFFS, "/input_adc.txt");     }
     return String();
 }
 
@@ -209,14 +221,26 @@ void setup()
             input_message = "No message sent";
             input_parameter = "none";
         }
-    
+       
         Serial.println(input_message);
         request->send(200, "text/html", 
         "neuen Wert eingelesen("+ input_parameter + ") with value: " + input_message + "<br><a href=\"/\">Return to Home Page</a>");
     });
     server.onNotFound(notFound);
+
+    
+    // Handle Web Server Events
+    events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+                       }
+    //-- send event with message "hello!", id current millis
+    //-- and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+                       });
+    server.addHandler(&events);
+    
     server.begin();
-  
 
 }
 
@@ -224,24 +248,44 @@ void loop()
 {
     static int x = FALSE; 
     static int heart = TRUE;
-    float adc0;
-    char text[20]; 
+    static int i = 0;
+    static int j = 0;
+    static float adcArray[N];
+    static float oldAdc;
+    static float newAdc;
+    static int secCounter = 0;
+    float sum;
+    
+    char text[LEN]; 
 
     int myInteger = read_file(SPIFFS, "/input_speedL.txt").toInt();
 
     if (oneSecFlag == TRUE)
     {
         oneSecFlag = FALSE;
+        secCounter++;
 
         heart = (heart == HIGH) ? LOW : HIGH;
         digitalWrite(LED, heart);
 
-        adc0 = analogRead(BATTERY_LEVEL) / REFV; 
-        sprintf(text,"%.2f",adc0);
-        //sprintf(text,"5.4");
-        write_file(SPIFFS, "/input_adc.txt", text);
+        // average:
+        adcArray[i] = analogRead(BATTERY_LEVEL)/ REFV;
+        i++;  
+        if (i == N) i = 0;
+        sum = 0;  
+        for (j = 0; j < N; j++) sum += adcArray[j];
+        newAdc = sum / (float)N;
 
-        // Serial.println(adc0); 
+        if (((newAdc - oldAdc) > 0.02) || (secCounter > 4))
+        {
+            sprintf(text, "send new event! %.2f Volt", newAdc);  
+            Serial.println(text);
+            oldAdc = newAdc;
+            secCounter = 0;
+           
+            events.send(String(newAdc).c_str(),"adc",millis());
+        } 
+        
     }
 }
 
@@ -261,11 +305,12 @@ void impuls_R_isr(void)
 
 void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 msec
 {
-    static int32_t tick = 0;
+    static int32_t tick = 0, tickR = 0;
     int l, r;
     
     
     tick++;
+    tickR++;
 
     if (tim) tim--;
 
@@ -281,19 +326,19 @@ void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 mse
     { 
         digitalWrite(MOTOR_L_DIR, LOW ); 
         l = +speedL; 
-        if ((tick & 0xff) < l) digitalWrite(MOTOR_L, HIGH); else digitalWrite(MOTOR_L, LOW);
+        if ((tickR & 0xff) < l) digitalWrite(MOTOR_L, HIGH); else digitalWrite(MOTOR_L, LOW);
     }
     else            
     { 
         digitalWrite(MOTOR_L, LOW ); 
         l = -speedL; 
-        if ((tick & 0xff) < l) digitalWrite(MOTOR_L_DIR, HIGH); else digitalWrite(MOTOR_L_DIR, LOW);
+        if ((tickR & 0xff) < l) digitalWrite(MOTOR_L_DIR, HIGH); else digitalWrite(MOTOR_L_DIR, LOW);
     }
 #else
     if (speedL > 0) { digitalWrite(MOTOR_L_DIR, LOW ); l = +speedL; }
     else            { digitalWrite(MOTOR_L_DIR, HIGH); l = -speedL; }
 
-    if ((tick & 0xff) < l) digitalWrite(MOTOR_L, HIGH); else digitalWrite(MOTOR_L, LOW);
+    if ((tickR & 0xff) < l) digitalWrite(MOTOR_L, HIGH); else digitalWrite(MOTOR_L, LOW);
 #endif
 
     // RIGHT: 
@@ -302,19 +347,19 @@ void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 mse
     { 
         digitalWrite(MOTOR_R, LOW ); 
         r = -speedR; 
-        if ((tick & 0xff) < l) digitalWrite(MOTOR_R_DIR, HIGH); else digitalWrite(MOTOR_R_DIR, LOW);
+        if ((tickR & 0xff) < l) digitalWrite(MOTOR_R_DIR, HIGH); else digitalWrite(MOTOR_R_DIR, LOW);
     }
     else            
     { 
         digitalWrite(MOTOR_R_DIR, LOW ); 
         r = +speedR; 
-        if ((tick & 0xff) < l) digitalWrite(MOTOR_R, HIGH); else digitalWrite(MOTOR_R, LOW);
+        if ((tickR & 0xff) < l) digitalWrite(MOTOR_R, HIGH); else digitalWrite(MOTOR_R, LOW);
     }
 #else
     if (speedR > 0) { digitalWrite(MOTOR_R_DIR, HIGH); r = +speedR; }
     else            { digitalWrite(MOTOR_R_DIR, LOW ); r = -speedR; }
 
-    if ((tick & 0xff) < r) digitalWrite(MOTOR_R, HIGH); else digitalWrite(MOTOR_R, LOW);
+    if ((tickR & 0xff) < r) digitalWrite(MOTOR_R, HIGH); else digitalWrite(MOTOR_R, LOW);
 #endif
 
 
